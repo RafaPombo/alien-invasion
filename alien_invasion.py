d7c8cf64da756a74d52a8cf757e5bac47328918d
Rafael <rafinha.2014.rafa.el.rr@gmail.com>
2021-03-06 18:30:59 -0300

    Fixed aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -263,7 +263,7 @@ class AlienInvasion:
         alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
-        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        alien.rect.y = 50 + alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _check_fleet_edges(self):
